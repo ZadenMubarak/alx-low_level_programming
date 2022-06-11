a0b5012c3f3e32a50a4261b9da6957657fa7322e
ZadenMubarak <zaden.ngobeni@gmail.com>
2022-06-10 21:47:14 -0700

    _isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,8 +6,16 @@ int main(void)
     char c;
 
     c = 'A';
-    printf("%c: %d\n", c, isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, isupper(c));
-    return (0);
+    if (isupper(c)){
+        printf("1");
+        return (1);
+    }
+    else{
+        printf("0");
+        return (0);
+    }
+    // printf("%c: %d\n", c, isupper(c));
+    // c = 'a';
+    // printf("%c: %d\n", c, isupper(c));
+    return 0;
 }
